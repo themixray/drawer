23ed86565b04b5ab14aedf9a9c0902793e6c2fbc
TheMixRay <35273590+themixray@users.noreply.github.com>
2022-02-24 11:46:29 +0300

    Add files via upload

--- paint.py
+++ paint.py
@@ -1,3 +1,4 @@
+from tkinter import colorchooser,Tk
 from drawer import color_symbol
 from colored import fg,bg,attr
 from console import console
@@ -27,7 +28,9 @@ def reload():
         for x in range(62):
             win.set(x+1,y+1," ",False)
     for i in btns:
-        win.write(i[1],i[2],i[0],False)
+        win.set(i[1],i[2],i[0][0],False)
+        win.write(i[1]+1,i[2],i[0][1:][:-1],False)
+        win.set(i[1]+len(i[0])-1,i[2],i[0][-1],False)
     win.update()
 
 
@@ -37,8 +40,16 @@ btns = []
 def btn(text,x,y,cb):
     btns.append(["["+text.upper()+"]",x,y+32,cb,False])
 
-
 btn("CLEAR",1,1,reload)
+
+color = "white"
+
+def choose_color():
+    global color
+    Tk().withdraw()
+    color = colorchooser.askcolor(title="Choose color")[1]
+
+btn("CHOOSE COLOR",10,1,choose_color)
 
 
 # -------   main loop   -------
@@ -54,17 +65,21 @@ while run:
         pos = (round(pos[0]),round(pos[1]))
         if mouse.is_pressed("left"):
             if pos[0] > 1 and pos[1] > 0 and pos[0] < 63 and pos[1] < 31:
-                win.set(pos[0],pos[1],"■")
+                win.set(pos[0],pos[1],color_symbol(color))
             for i in btns:
                 if pos[0] >= i[1] and pos[0] <= i[1]+len(i[0]) \
                             and pos[1] == i[2]+1 and not i[4]:
-                    win.write(i[1],i[2],fg("black")+bg("white")+i[0]+attr("reset"))
+                    win.set(i[1],i[2],fg("black")+bg("white")+i[0][0],False)
+                    win.write(i[1]+1,i[2],i[0][1:][:-1],False)
+                    win.set(i[1]+len(i[0])-1,i[2],i[0][-1]+attr("reset"),False)
                     i[4] = True
         elif mouse.is_pressed("right"):
             win.set(pos[0],pos[1]," ")
         else:
             for i in btns:
                 if i[4]:
-                    win.write(i[1],i[2],i[0])
+                    win.set(i[1],i[2],i[0][0],False)
+                    win.write(i[1]+1,i[2],i[0][1:][:-1],False)
+                    win.set(i[1]+len(i[0])-1,i[2],i[0][-1],False)
                     i[4] = False
                     i[3]()
